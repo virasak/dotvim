a03a1a3dc2089535e3dc939845a995dfa0fa54cd
vdungsrikaew <virasak.dungsrikaew@agoda.com>
2017-12-15 16:31:59 +0700

    move shell script out into its own file

--- README.md
+++ README.md
@@ -1,11 +1,5 @@
+# Setup
+
 ``` sh
-mkdir bundle
-cd bundle
-git clone https://github.com/scrooloose/nerdtree.git
-git clone https://github.com/ctrlpvim/ctrlp.vim.git
-git clone https://github.com/tpope/vim-surround.git
-git clone https://github.com/tpope/vim-repeat.git
-git clone https://github.com/tpope/vim-unimpaired.git
-git clone https://github.com/itchyny/lightline.vim.git
-git clone https://github.com/ap/vim-buftabline.git
+$ ./setup.sh
 ```
---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+mkdir bundle
+cd bundle
+git clone https://github.com/ap/vim-buftabline.git
+git clone https://github.com/ctrlpvim/ctrlp.vim.git
+git clone https://github.com/itchyny/lightline.vim.git
+git clone https://github.com/scrooloose/nerdtree.git
+git clone https://github.com/tpope/vim-fugitive.git
+git clone https://github.com/tpope/vim-repeat.git
+git clone https://github.com/tpope/vim-surround.git
+git clone https://github.com/tpope/vim-unimpaired.git
