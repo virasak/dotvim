cdada46e08c9c9cf7b8b2001ce829c95d37c7869
vdungsrikaew <virasak.dungsrikaew@agoda.com>
2018-01-10 22:00:05 +0700

    add typescript-vim plugin

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ cd bundle
 git clone https://github.com/ap/vim-buftabline.git
 git clone https://github.com/ctrlpvim/ctrlp.vim.git
 git clone https://github.com/itchyny/lightline.vim.git
+git clone https://github.com/leafgarland/typescript-vim.git
 git clone https://github.com/scrooloose/nerdtree.git
 git clone https://github.com/tpope/vim-fugitive.git
 git clone https://github.com/tpope/vim-repeat.git
