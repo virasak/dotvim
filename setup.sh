aea21229deff355e24ff8163f04759c11aeaaabf
Virasak Dungsrikaew <virasak@omise.co>
2019-09-27 20:44:48 +0700

    Remove unused plugin

--- setup.sh
+++ setup.sh
@@ -4,13 +4,11 @@ cd bundle
 git clone https://github.com/ap/vim-buftabline.git
 git clone https://github.com/ctrlpvim/ctrlp.vim.git
 git clone https://github.com/itchyny/lightline.vim.git
-git clone https://github.com/leafgarland/typescript-vim.git
 git clone https://github.com/scrooloose/nerdtree.git
 git clone https://github.com/tpope/vim-fugitive.git
 git clone https://github.com/tpope/vim-repeat.git
 git clone https://github.com/tpope/vim-surround.git
 git clone https://github.com/tpope/vim-unimpaired.git
-git clone https://github.com/drmingdrmer/xptemplate.git
 git clone https://github.com/virasak/vim-bufswitcher.git
 git clone https://github.com/elixir-editors/vim-elixir.git
 git clone https://github.com/junegunn/fzf.vim.git
